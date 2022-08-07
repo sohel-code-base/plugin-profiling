75756b38dc8731c0f10add3dea6e05d78fdda2a3
soheldeveloper <sohelrananayanpur@gmail.com>
2022-08-08 04:23:29 +0600

    add active version and download history

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,5 @@
-import './bootstrap';
-import './@amcharts';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import '@amcharts/amcharts5';
+// import 'jquery/dist/jquery.min.js'
+// import 'bootstrap-datetime-picker/css/bootstrap-datetimepicker.min.css';
+// import 'bootstrap-datetime-picker/js/bootstrap-datetimepicker.min.js';
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/plugin-details',[DashboardController::class, 'plugin_details'])->name('plugin.details');
+Route::get('/',[DashboardController::class, 'index']);
